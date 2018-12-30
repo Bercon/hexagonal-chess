8f6e45edc61b6cfda4208cd14ec0f3cb838dbf88
Jerry Ylilammi <jerry@ylilammi.com>
2018-12-30 19:30:57 +0200

    Primitive bruteforce solver done

--- src/queens.js
+++ src/queens.js
@@ -1,40 +1,56 @@
-console.log('Hexagonal Chess: 2N-1 Queens');
+const N = parseInt(process.argv[2] || '3');
 
-const N = 3;
+console.log('Hexagonal Chess: 2N-1 Queens for N =', N);
 
 const hexes = [];
-
 
 for (let x = -N; x <= N; x++) {
   let ys = x < 0 ? N : N - x;
   let ye = x < 0 ? -N - x : -N;
   for (let y = ys; y >= ye; y--) {
-    //console.log(x, y, -y - x);
     hexes.push([x, y, -y - x]);
   }
-  //console.log('');
 }
 
-
-function cubeToOffsetCoordinates(hex) {
-  var col = hex[0];
-  var row = hex[2] + (hex[0] - (hex[0] & 1)) / 2;
-  console.log(hex.join(), [col + N, row + N].join());
-  return [col + N, row + N];
+function filterHexes(hexes, queen) {
+  return hexes.filter(function(hex) {
+    return !(hex[0] === queen[0] || hex[1] === queen[1] || hex[2] === queen[2])
+      && !(Math.abs(hex[0]-queen[0]) === Math.abs(hex[1]-queen[1]))
+      && !(Math.abs(hex[1]-queen[1]) === Math.abs(hex[2]-queen[2]))
+      && !(Math.abs(hex[2]-queen[2]) === Math.abs(hex[0]-queen[0]))
+  });
 }
+
+let largestFound = [];
+
+function maxQueens(freeHexes, queens) {
+  for (let i = 0; i < freeHexes.length; i++) {
+    const hex = freeHexes[i];
+    const freeAfter = filterHexes(freeHexes, hex);
+    const queensAfter = [...queens, hex];
+    if (freeAfter.length > 0)
+      maxQueens(freeAfter, queensAfter);
+    else {
+      if (largestFound.length < queensAfter.length) {
+        largestFound = queensAfter;
+      }
+    }
+  }
+}
+
+maxQueens(hexes, []);
+
+console.log('Largest found:', largestFound);
+
 
 function alternate(even, odd, count) {
   const buffer = [];
   for (let x = 0; x < count ; x++) {
     if (x % 2 == 0)
       Array.prototype.push.apply(buffer, even.split(''));
-      //buffer.push(even.split(''));
     else
       Array.prototype.push.apply(buffer, odd.split(''));
-      //buffer.push(odd.split(''));
   }
-  //buffer.push('\n');
-  //return buffer.join('');
   return buffer;
 }
 
@@ -64,48 +80,35 @@ function writeBoard(board, offsetX, offsetY, str) {
       board[offsetY][offsetX + i] = arr[i];
 }
 
-function printHex(board, hex) {
+function printHex(board, hex, queen=false) {
   var col = hex[0] + N;
-  var row = hex[2] + (hex[0] + (hex[0] & 1)) / 2 + N;
-  console.log(col, row, 'hex', hex.join());
+  var row = N % 2 === 0 ? hex[2] + (hex[0] - (hex[0] & 1)) / 2 + N
+      : hex[2] + (hex[0] + (hex[0] & 1)) / 2 + N;
   var offsetX = 2 + col * 7;
   var offsetY = 1 + row * 4 + (col % 2) * 2;
-  //writeBoard(board, offsetX, offsetY, 'X=' + col);
-  //writeBoard(board, offsetX, offsetY + 1, 'Y=' + row);
-  //writeBoard(board, offsetX, offsetY, 'X=' + hex[0]);
-  //writeBoard(board, offsetX, offsetY + 1, 'Y=' + hex[1]);
-  //writeBoard(board, offsetX, offsetY + 2, 'Z=' + hex[2]);
-
-  writeBoard(board, offsetX, offsetY + 1, 'Queen');
+  if (queen) {
+    writeBoard(board, offsetX, offsetY, '     ');
+    writeBoard(board, offsetX, offsetY + 1, 'QUEEN');
+    writeBoard(board, offsetX, offsetY + 2, '     ');
+  } else {
+    //writeBoard(board, offsetX, offsetY, 'X=' + col);
+    //writeBoard(board, offsetX, offsetY + 1, 'Y=' + row);
+    writeBoard(board, offsetX, offsetY, 'X' + hex[0]);
+    writeBoard(board, offsetX, offsetY + 1, 'Y' + hex[1]);
+    writeBoard(board, offsetX, offsetY + 2, 'Z' + hex[2]);
+  }
 }
-
 
 
 for (let i = 0; i < hexes.length; i++) {
   printHex(board, hexes[i]);
 }
 
+for (let i = 0; i < largestFound.length; i++) {
+  printHex(board, largestFound[i], true);
+}
+
 
 for (let i = 0; i < board.length; i++) {
   console.log(board[i].join(''));
 }
-
-/*
-
-
-x   0      1       2       3     4
-
-y=0
-2
-
-  _____         _____
- /     \       /     \
-/       \_____/       \
-\       /     \       /
- \_____/       \_____/
-       \       /
-        \_____/
-
-
-
-*/
